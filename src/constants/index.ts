4c20e2e8e68c29cc722617cfb15507c141404b6a
Canh Trinh <canh@axelar.network>
2021-09-14 16:13:05 -0400

    feat(ui): added ChainSelector UI components for to/from chains #10

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export interface ISupportedChainType {
+	symbol: string;
+	name: string;
+}
+
+export type ISupportedChainList = ISupportedChainType[];
+
+export const SupportedChains: ISupportedChainList = [
+	{ symbol: "AXE", name: "Axelar" },
+	{ symbol: "BTC", name: "Bitcoin"  },
+	{ symbol: "ETH", name: "Ethereum" }
+]
+
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * from "./constants";
 export * from "./interface";
 export * from "./server-connect";
