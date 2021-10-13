aacac79148fc0d567275ff0906f641ead5c770d2
Canh Trinh <canhtrinh@users.noreply.github.com>
2021-10-13 10:09:13 -0400

    feat: wire up transferAssets endpoint in rest-server to connect to microservices lambda #10 (#17)
    * feat(rest): connecting transferAsset endpoint to devnet broadcast lambda (which is still under dev) #10
    
    * fix(rest): reverting axelarjs-sdk dependency
    
    * fix(rest): cleaning up microservices url endpoint
    
    * chore(sdk): bumping sdk version
    
    * chore(ui): removing unused variable in ChainSelector.tsx
    
    * Update axelarjs-rest-server/src/Plugins/client-rest-routes/helpers.ts
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    
    * Update axelarjs-sdk/src/interface/IAssetTransferObject.ts
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    
    * chore(rest): deleting local .env file
    
    * chore(sdk): renaming link request types
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>

--- src/interface/IAssetTransferObject.ts
+++ src/interface/IAssetTransferObject.ts
@@ -6,3 +6,21 @@ export interface IAssetTransferObject {
 	sourceAsset: IAsset;
 	destinationTokenInfo: ITokenAddress;
 }
+
+export enum LinkType {
+	BITCOIN = "/bitcoin.v1beta1.LinkRequest",
+	EVM = "/evm.v1beta1.LinkRequest"
+}
+
+export interface ILinkRequestBody {
+	"@type": LinkType;
+	"sender": string;
+	"recipient_addr": string;
+	"recipient_chain": string;
+}
+
+export interface IBTCLinkRequestBody extends ILinkRequestBody {}
+export interface IEVMLinkRequestBody extends ILinkRequestBody {
+	"chain": string; //source chain
+	"asset": string;
+}
