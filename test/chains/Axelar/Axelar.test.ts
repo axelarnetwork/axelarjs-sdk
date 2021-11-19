51838b678a9e689c09949d54368e556b5594e850
Canh Trinh <canh@axelar.network>
2021-11-19 13:56:50 -0500

    test: adding unit tests for address validator

---
+++ test/chains/Axelar/Axelar.test.ts
@@ -0,0 +1,33 @@
+import Axelar       from "../../../src/chains/Axelar";
+import {IAssetInfo} from "../../../src/interface";
+
+let axelarChain: Axelar;
+let asset: IAssetInfo;
+
+beforeEach(() => {
+	axelarChain = new Axelar();
+	asset = { assetAddress: "" };
+})
+
+test('destination address validation', () => {
+
+	const addressMap: { [key: string]: string} = {
+		validAddr45: "axelar1cupayrv3fzgzn3mjg38d6qxqey64rw9fmkkp5f",
+		invalidAddr45: "axelar1cupayrv3fzgzn3mjg38d6qxqey64rw9fmkkp5d",
+		validAddr65: "axelar1386l7f7uyjejklmnzvsnkhffus625pj7eve85gx6z7696d7ujlss3ras55",
+		invalidAddr65: "axelar1386l7f7uyjejklmnzvsnkhffus625pj7eve85gx6z7696d7ujlss3ras54",
+	}
+
+	asset.assetAddress = addressMap.validAddr45;
+	expect(axelarChain.validateAddress(asset)).toBe(true);
+
+	asset.assetAddress = addressMap.invalidAddr45;
+	expect(axelarChain.validateAddress(asset)).toBe(false);
+
+	asset.assetAddress = addressMap.validAddr65;
+	expect(axelarChain.validateAddress(asset)).toBe(true);
+
+	asset.assetAddress = addressMap.invalidAddr65;
+	expect(axelarChain.validateAddress(asset)).toBe(false);
+
+});
