e455e7850af4e74c3d02bfee68b6a8b6cae22130
Canh Trinh <canh@axelar.network>
2021-12-08 13:50:10 -0500

    chore: cleaning up types and updating to websocket provider instead

---
+++ src/chains/Ethereum/ethersjsProvider.ts
@@ -0,0 +1,19 @@
+import {ethers} from "ethers";
+
+type ProviderType =
+	| 'jsonRPC'
+	| 'infura'
+	| 'infuraWS'
+	| 'ropsten';
+
+const providers: { [key: string]: (url?: string) => ethers.providers.BaseProvider } = {};
+
+providers.ropsten = (url?: string) => ethers.getDefaultProvider('ropsten');
+providers.infura = (url?: string) => new ethers.providers.InfuraProvider('ropsten');
+providers.infuraWS = (url?: string) => new ethers.providers.WebSocketProvider(url || "");
+
+export const getEthersJsProvider = (providerType: ProviderType) => {
+	if (providerType === 'infuraWS')
+		return providers[providerType]("https://ropsten.infura.io/v3/2be110f3450b494f8d637ed7bb6954e3");
+	return providers[providerType]();
+}
