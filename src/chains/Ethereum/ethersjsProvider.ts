cdbe30108ddb1968fdd3c9dcf5d7a355d234e6d3
Canh Trinh <canh@axelar.network>
2021-12-13 14:21:46 -0500

    chore: fixing ethersjs provider

--- src/chains/Ethereum/ethersjsProvider.ts
+++ src/chains/Ethereum/ethersjsProvider.ts
@@ -10,10 +10,10 @@ const providers: { [key: string]: (url?: string) => ethers.providers.BaseProvide
 
 providers.ropsten = (url?: string) => ethers.getDefaultProvider('ropsten');
 providers.infura = (url?: string) => new ethers.providers.InfuraProvider('ropsten');
-providers.infuraWS = (url?: string) => new ethers.providers.WebSocketProvider(url || "");
+providers.infuraWS = (url?: string) => new ethers.providers.WebSocketProvider(url || "", "ropsten");
 
 export const getEthersJsProvider = (providerType: ProviderType) => {
 	if (providerType === 'infuraWS')
-		return providers[providerType]("https://ropsten.infura.io/v3/2be110f3450b494f8d637ed7bb6954e3");
+		return providers[providerType]("wss://ropsten.infura.io/ws/v3/2be110f3450b494f8d637ed7bb6954e3");
 	return providers[providerType]();
 }
