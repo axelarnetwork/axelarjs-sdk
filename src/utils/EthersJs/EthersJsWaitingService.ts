a3ad5ff57f5b8e6ea55cb08b6b8827fa9307a31a
Canh Trinh <canh@axelar.network>
2021-12-13 22:27:03 -0500

    chore: code cleanup

--- src/utils/EthersJs/EthersJsWaitingService.ts
+++ src/utils/EthersJs/EthersJsWaitingService.ts
@@ -32,8 +32,7 @@ export default class EthersJsWaitingService extends BaseWaitingService implement
 	public async wait(address: string, cb: any): Promise<any> {
 		return new Promise((resolve, reject) => {
 			this.tokenContract.once(this.filter, (from: any, to: any, amount: any, event: any) => {
-
-				console.log(`Incoming amount of: ${(amount)}, from: ${from}.`, event);
+				console.log(`Incoming amount of: ${formatEther(amount)}, from: ${from}.`, event);
 				event.axelarRequiredNumConfirmations = this.numConfirmations;
 				cb(event);
 				resolve(event);
@@ -43,7 +42,6 @@ export default class EthersJsWaitingService extends BaseWaitingService implement
 
 	private async init(chainInfo: IChainInfo, assetInfo: IAssetInfo, environment: string, providerType: ProviderType) {
 
-		debugger;
 		const configs: IEnvironmentConfigs = getConfigs(environment);
 		const { tokenAddressMap } = (configs as any)[chainInfo.chainName.toLowerCase()];
 		const tokenSymbol: keyof IEthersJsTokenMap = assetInfo.assetSymbol as keyof IEthersJsTokenMap;
