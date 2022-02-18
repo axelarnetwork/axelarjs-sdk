8dce45f60ddd859c91087960a9f51b328068d02e
npty <nptytn2@gmail.com>
2022-02-18 17:35:48 +0700

    init eslint and prettier config

--- src/TransferAssetBridge/status/index.ts
+++ src/TransferAssetBridge/status/index.ts
@@ -1,32 +1,42 @@
 import {
-	AssetInfo,
-	BlockchainWaitingService,
-	BlockchainWaitingServiceFinder,
-	Chain,
-	ChainInfo,
-	SourceOrDestination
-}                  from "../../interface";
-import {ChainList} from "../../chains";
+  AssetInfo,
+  BlockchainWaitingService,
+  BlockchainWaitingServiceFinder,
+  Chain,
+  ChainInfo,
+  SourceOrDestination,
+} from "../../interface";
+import { ChainList } from "../../chains";
 
-const waitingServiceMap: { [chainKey: string]: BlockchainWaitingServiceFinder } = {};
+const waitingServiceMap: {
+  [chainKey: string]: BlockchainWaitingServiceFinder;
+} = {};
 
 ChainList.forEach((chainInfo: Chain) => {
-	const chainKey: string = chainInfo.chainInfo.chainSymbol.toLowerCase();
-	waitingServiceMap[chainKey] = chainInfo.waitingService as BlockchainWaitingServiceFinder
+  const chainKey: string = chainInfo.chainInfo.chainSymbol.toLowerCase();
+  waitingServiceMap[chainKey] =
+    chainInfo.waitingService as BlockchainWaitingServiceFinder;
 });
 
-type IGetWaitingService = (chainInfo: ChainInfo,
-                           assetInfo: AssetInfo,
-                           sOrDChain: SourceOrDestination,
-                           environment: string) => BlockchainWaitingService | Promise<BlockchainWaitingService>;
+type IGetWaitingService = (
+  chainInfo: ChainInfo,
+  assetInfo: AssetInfo,
+  sOrDChain: SourceOrDestination,
+  environment: string
+) => BlockchainWaitingService | Promise<BlockchainWaitingService>;
 const getWaitingService: IGetWaitingService = (
-	chainInfo: ChainInfo,
-	assetInfo: AssetInfo,
-	sOrDChain: SourceOrDestination,
-	environment: string
+  chainInfo: ChainInfo,
+  assetInfo: AssetInfo,
+  sOrDChain: SourceOrDestination,
+  environment: string
 ) => {
-	const chainKey = chainInfo.chainSymbol.toLowerCase();
-	return waitingServiceMap[chainKey](chainInfo, assetInfo, sOrDChain, environment);
+  const chainKey = chainInfo.chainSymbol.toLowerCase();
+  return waitingServiceMap[chainKey](
+    chainInfo,
+    assetInfo,
+    sOrDChain,
+    environment
+  );
 };
 
 export default getWaitingService;
