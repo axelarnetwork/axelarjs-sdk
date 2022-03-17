bdb9809a5e2e1a5d4b12734344a7c077d75ceb06
vladwulf <agaev.vladimir@gmail.com>
2022-03-17 19:59:24 +0000

    chore: add e2e tests

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,3 +1,49 @@
+import { Wallet } from "ethers";
+import { createWallet, signOtc } from "../../src/utils";
+import { TransferAssetBridge } from "../../src/libs";
+import { OTC } from "../../src/services/types";
+
 describe("E2E", () => {
-  it.todo("should pass");
+  let axelar: TransferAssetBridge;
+  let wallet: Wallet;
+  let signature: string;
+  let validationMsg: string;
+
+  beforeAll(() => {
+    axelar = new TransferAssetBridge("testnet");
+  });
+
+  describe("wallet creation", () => {
+    beforeAll(() => {
+      wallet = createWallet();
+    });
+
+    it("should create wallet", () => {
+      expect(wallet).toBeDefined();
+    });
+  });
+
+  describe("getting one time code", () => {
+    let response: OTC;
+    beforeAll(async () => {
+      response = await axelar.getOneTimeCode(wallet.address, "0x");
+    });
+
+    it("should get otc", () => {
+      expect(response.otc).toBeDefined(); // FIXME: check why otc is "OK"
+      expect(response.validationMsg).toBeDefined();
+      validationMsg = response.validationMsg;
+    });
+  });
+
+  describe("signin one time code", () => {
+    beforeAll(async () => {
+      signature = await signOtc(wallet, validationMsg);
+    });
+    it("should sign message", () => {
+      expect(signature).toBeDefined();
+    });
+  });
+
+  describe("getting deposit address", () => {});
 });
