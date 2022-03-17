669fd864152892fad1f873e49d5f94bad01f4c84
vladwulf <agaev.vladimir@gmail.com>
2022-03-17 18:52:49 +0000

    chore: fix loadChains internal imports

--- src/utils/getWaitingService.ts
+++ src/utils/getWaitingService.ts
@@ -1,5 +1,5 @@
 import { AssetInfo } from "../assets/types";
-import { ChainList } from "../chains";
+import { loadChains } from "../chains";
 import {
   BlockchainWaitingServiceFinder,
   Chain,
@@ -7,16 +7,6 @@ import {
   BlockchainWaitingService,
 } from "../chains/types";
 import { SourceOrDestination } from "../services/types";
-
-const waitingServiceMap: {
-  [chainKey: string]: BlockchainWaitingServiceFinder;
-} = {};
-
-ChainList.forEach((chainInfo: Chain) => {
-  const chainKey: string = chainInfo.chainInfo.chainSymbol.toLowerCase();
-  waitingServiceMap[chainKey] =
-    chainInfo.waitingService as BlockchainWaitingServiceFinder;
-});
 
 type IGetWaitingService = (
   chainInfo: ChainInfo,
@@ -31,6 +21,20 @@ export const getWaitingService: IGetWaitingService = (
   sOrDChain: SourceOrDestination,
   environment: string
 ) => {
+  const waitingServiceMap: {
+    [chainKey: string]: BlockchainWaitingServiceFinder;
+  } = {};
+
+  const chains = loadChains({
+    environment,
+  });
+
+  chains.forEach((chainInfo: Chain) => {
+    const chainKey: string = chainInfo.chainInfo.chainSymbol.toLowerCase();
+    waitingServiceMap[chainKey] =
+      chainInfo.waitingService as BlockchainWaitingServiceFinder;
+  });
+
   const chainKey = chainInfo.chainSymbol.toLowerCase();
   return waitingServiceMap[chainKey](
     chainInfo,
