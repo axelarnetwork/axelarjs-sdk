7e77d63e4ae92d324c403946db6318ac6238ade2
Canh Trinh <canh@axelar.network>
2022-03-16 21:18:04 -0400

    chore: adding juno chain

---
+++ src/chains/Juno/index.ts
@@ -0,0 +1,34 @@
+import { AssetInfo, Chain, ChainInfo } from "../../interface";
+import { bech32 } from "bech32";
+import Axelar from "../Axelar";
+
+export default class Juno extends Axelar implements Chain {
+  public chainInfo: ChainInfo = {
+    chainSymbol: "JUNO",
+    chainName: "Juno",
+    estimatedWaitTime: 5,
+    fullySupported: false,
+    assets: [],
+    txFeeInPercent: 0.1,
+    module: "axelarnet",
+    chainIdentifier: {
+      devnet: "juno",
+      testnet: "juno",
+      mainnet: "juno",
+    },
+  };
+
+  public validateAddress = (addressInfo: AssetInfo): boolean => {
+    if (!addressInfo?.assetAddress) return false;
+
+    try {
+      return bech32.decode(addressInfo.assetAddress).prefix === "juno";
+    } catch (e) {
+      return false;
+    }
+  };
+
+  constructor() {
+    super();
+  }
+}
