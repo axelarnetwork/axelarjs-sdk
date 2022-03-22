364d50aaff7a3441031d0f7474799f76b157af76
vladwulf <agaev.vladimir@gmail.com>
2022-03-22 15:08:51 +0000

    chore: add wallet saving to localStorage

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,8 +1,23 @@
 import { ethers, Wallet } from "ethers";
 
 export function createWallet() {
-  const wallet = ethers.Wallet.createRandom();
-  return wallet;
+  if (globalThis.localStorage) {
+    const mnemonic = globalThis.localStorage.getItem("axelar-wallet");
+    if (mnemonic) {
+      const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+      return wallet;
+    } else {
+      const wallet = ethers.Wallet.createRandom();
+      globalThis.localStorage.setItem(
+        "axelar-wallet",
+        wallet._mnemonic().phrase
+      );
+      return wallet;
+    }
+  } else {
+    const wallet = ethers.Wallet.createRandom();
+    return wallet;
+  }
 }
 
 export function signOtc(wallet: Wallet, message: string) {
