4c46055dde0a4e071ecc8ffc53a61ef94266891a
vladwulf <agaev.vladimir@gmail.com>
2022-04-04 14:19:13 +0100

    chore: hide waiting service

--- src/chains/Axelar/WaitingService.ts
+++ src/chains/Axelar/WaitingService.ts
@@ -16,32 +16,32 @@ export default class WaitingService
     super(1, assetInfo.assetAddress as string);
   }
 
-  public async waitForLinkEvent(
-    roomId: string,
-    interimStatusCb: StatusResponse,
-    clientSocketConnect: SocketService
-  ) {
-    return this.waitForEvent(roomId, interimStatusCb, clientSocketConnect);
-  }
+  // public async waitForLinkEvent(
+  //   roomId: string,
+  //   interimStatusCb: StatusResponse,
+  //   clientSocketConnect: SocketService
+  // ) {
+  //   return this.waitForEvent(roomId, interimStatusCb, clientSocketConnect);
+  // }
 
-  public async waitForDepositConfirmation(
-    roomId: string,
-    interimStatusCb: StatusResponse,
-    clientSocketConnect: SocketService
-  ) {
-    return this.waitForEvent(roomId, interimStatusCb, clientSocketConnect);
-  }
+  // public async waitForDepositConfirmation(
+  //   roomId: string,
+  //   interimStatusCb: StatusResponse,
+  //   clientSocketConnect: SocketService
+  // ) {
+  //   return this.waitForEvent(roomId, interimStatusCb, clientSocketConnect);
+  // }
 
-  public async waitForTransferEvent(
-    assetAndChainInfo: AssetAndChainInfo,
-    interimStatusCb: StatusResponse,
-    clientSocketConnect: SocketService,
-    roomId: string
-  ) {
-    return this.waitForEvent(
-      roomId as string,
-      interimStatusCb,
-      clientSocketConnect
-    );
-  }
+  // public async waitForTransferEvent(
+  //   assetAndChainInfo: AssetAndChainInfo,
+  //   interimStatusCb: StatusResponse,
+  //   clientSocketConnect: SocketService,
+  //   roomId: string
+  // ) {
+  //   return this.waitForEvent(
+  //     roomId as string,
+  //     interimStatusCb,
+  //     clientSocketConnect
+  //   );
+  // }
 }
