949d418e0398a1f25917ab2d2dabf41d780e82fc
vladwulf <agaev.vladimir@gmail.com>
2022-04-06 12:20:36 +0100

    clean: remove unused methods

--- src/services/SocketService.ts
+++ src/services/SocketService.ts
@@ -1,5 +1,4 @@
 import { io, Socket } from "socket.io-client";
-import { SocketListenerTypes } from "../chains/types";
 
 export class SocketService {
   private socket: Socket;
@@ -54,52 +53,6 @@ export class SocketService {
           this.disconnect();
         });
       });
-    });
-  }
-
-  public joinRoomAndWaitDepositConfirmationEvent(roomId: string, waitCb: any) {
-    return new Promise(async (resolve) => {
-      await this.createSocket();
-      const ms = 1.8e6; //30 minutes
-      const timeout = setTimeout(() => {
-        waitCb({ timedOut: true });
-        this.disconnect();
-      }, ms);
-      this.socket.emit("room:join", roomId, () => {
-        this.socket.on("bridge-event", (data: any) => {
-          waitCb && waitCb(data);
-          resolve(data);
-          clearTimeout(timeout);
-          this.disconnect();
-        });
-      });
-    });
-  }
-
-  public emitMessageAndWaitForReply(
-    triggerTopic: SocketListenerTypes,
-    message: any,
-    waitTopic: SocketListenerTypes,
-    waitCb: any
-  ) {
-    return new Promise(async (resolve, reject) => {
-      await this.createSocket();
-      this.emitMessage(triggerTopic, message);
-      this.awaitResponse(waitTopic, (data: any) => {
-        waitCb(data);
-        resolve(data);
-      });
-    });
-  }
-
-  public emitMessage(topic: SocketListenerTypes, message: any): void {
-    this.socket?.emit(topic, message);
-  }
-
-  public awaitResponse(topic: SocketListenerTypes, waitCb: any): void {
-    this.socket?.on(topic, (data: any) => {
-      waitCb && waitCb(data);
-      this.disconnect();
     });
   }
 
