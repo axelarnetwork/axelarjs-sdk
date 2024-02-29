784458753d3af8971562eb19ea4689a63109d0ab
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-02-28 20:31:09 -0500

    chore: optimize l2 gas calculations for estimateGasFee (#300)

--- src/utils/isL2Chain.ts
+++ src/utils/isL2Chain.ts
@@ -1,4 +1,3 @@
-import { loadChains } from "../chains";
 import { Environment, FeeToken } from "../libs";
 
 export async function isL2Chain(
@@ -6,7 +5,5 @@ export async function isL2Chain(
   chain: string,
   destinationNativeToken: FeeToken
 ) {
-  const chains = await loadChains({ environment });
-  const evmChains = chains.filter((chain) => chain.module === "evm").map((chain) => chain.id);
-  return destinationNativeToken.l1_gas_price_in_units && evmChains.includes(chain);
+  return destinationNativeToken.l1_gas_price_in_units;
 }
