df71d803cd633dd8410838f39c51a862cc3debc5
npty <nptytn2@gmail.com>
2024-03-08 20:13:30 +0700

    chore: fix number overflow

--- src/libs/AxelarQueryAPI.ts
+++ src/libs/AxelarQueryAPI.ts
@@ -310,7 +310,7 @@ export class AxelarQueryAPI {
       const ethTokenPrice = Number(ethereumToken.token_price.usd);
       const ethToSrcTokenPriceRatio = ethTokenPrice / srcTokenPrice;
 
-      const actualL1ExecutionFee = Math.ceil(l1ExecutionFee.toNumber() * ethToSrcTokenPriceRatio);
+      const actualL1ExecutionFee = l1ExecutionFee.mul(ethToSrcTokenPriceRatio * 1000).div(1000);
 
       l1ExecutionFee = BigNumber.from(actualL1ExecutionFee.toString());
 
