4863c339734c585c82fa7e3c63b032452c5b832d
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-04-18 08:24:18 -0400

    chore: fix decimals issue with l2 dest chains (#315)
    * chore: fix decimals issue with l2 dest chains
    
    chore: delete unused test
    
    * 0.16.0-alpha.1
    
    * chore: improve convertTokenAmount impl
    
    * chore: refactor bignumber amount conversion
    
    ---------
    
    Co-authored-by: npty <nptytn2@gmail.com>

--- src/libs/BigNumberUtils.ts
+++ src/libs/BigNumberUtils.ts
@@ -16,4 +16,12 @@ export class BigNumberUtils {
   public static divideToGetWei(bn: BigNumber | string, number: string, units: number): BigNumber {
     return BigNumber.from(bn).div(parseUnits(number, units));
   }
+
+  public static convertTokenAmount(
+    ethAmount: BigNumber,
+    sourceDecimals: number,
+    targetDecimals: number
+  ) {
+    return ethAmount.mul(parseUnits("1", targetDecimals)).div(parseUnits("1", sourceDecimals));
+  }
 }
