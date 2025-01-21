e7b7f0a7505cb32dcea6bfc5f86a8e06f6494d8a
João Sousa <joaosousa@Joaos-MBP.home>
2025-01-21 23:05:58 +0000

    export QueryClient class

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,6 +2,7 @@ export * from "./AxelarAssetTransfer";
 export * from "./AxelarGateway";
 export * from "./AxelarQueryAPI";
 export * from "./AxelarSigningClient";
+export * from "./AxelarQueryClient";
 export * from "./TransactionRecoveryApi";
 export * from "./GatewayTx";
 export * from "./types";
