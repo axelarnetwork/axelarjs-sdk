765f0d2db1c0ac2401c0cc27deb825f6e2efef13
João Sousa <joaosousa@Joaos-MBP.home>
2025-01-23 22:51:20 +0000

    addendum

---
+++ src/libs/AxelarSigningClient/types/NexusTxTypes.ts
@@ -0,0 +1,30 @@
+import {
+  RegisterChainMaintainerRequest,
+  DeregisterChainMaintainerRequest,
+  ActivateChainRequest,
+  DeactivateChainRequest,
+  RegisterAssetFeeRequest,
+  SetTransferRateLimitRequest,
+  protobufPackage,
+} from "@axelar-network/axelarjs-types/axelar/nexus/v1beta1/tx";
+
+import { Registry } from "@cosmjs/proto-signing";
+
+const TxTypeUrlMap = {
+  RegisterChainMaintainerRequest: `/${protobufPackage}.RegisterChainMaintainerRequest`,
+  DeregisterChainMaintainerRequest: `/${protobufPackage}.DeregisterChainMaintainerRequest`,
+  ActivateChainRequest: `/${protobufPackage}.ActivateChainRequest`,
+  DeactivateChainRequest: `/${protobufPackage}.DeactivateChainRequest`,
+  RegisterAssetFeeRequest: `/${protobufPackage}.RegisterAssetFeeRequest`,
+  SetTransferRateLimitRequest: `/${protobufPackage}.SetTransferRateLimitRequest`,
+
+};
+
+export const registerNexusTxTypes = (registry: Registry) => {
+  registry.register(TxTypeUrlMap.RegisterChainMaintainerRequest, RegisterChainMaintainerRequest);
+  registry.register(TxTypeUrlMap.DeregisterChainMaintainerRequest, DeregisterChainMaintainerRequest);
+  registry.register(TxTypeUrlMap.ActivateChainRequest, ActivateChainRequest);
+  registry.register(TxTypeUrlMap.DeactivateChainRequest, DeactivateChainRequest);
+  registry.register(TxTypeUrlMap.RegisterAssetFeeRequest, RegisterAssetFeeRequest);
+  registry.register(TxTypeUrlMap.SetTransferRateLimitRequest, SetTransferRateLimitRequest);
+};
