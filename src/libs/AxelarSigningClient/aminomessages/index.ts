08383c121167adb28f055ac898690fedc88505a8
João Sousa <joaosousa@Joaos-MBP.home>
2025-01-27 17:24:43 +0000

    prettier

--- src/libs/AxelarSigningClient/aminomessages/index.ts
+++ src/libs/AxelarSigningClient/aminomessages/index.ts
@@ -7,56 +7,52 @@ import { AXELAR_PREFIX } from "../const";
 import Long from "long";
 
 export const createAxelarAminoConverters = (): AminoConverters => ({
+  // nexus module
+  "/axelar.nexus.v1beta1.SetTransferRateLimitRequest": {
+    aminoType: "nexus/SetTransferRateLimit",
+    toAmino: ({ sender, chain, limit: { amount, denom }, window: { seconds, nanos } }) => ({
+      sender: toBech32(AXELAR_PREFIX, sender),
+      chain,
+      window: Long.fromValue(seconds).multiply(1000000000).add(nanos).toString(),
+      limit: {
+        amount,
+        denom,
+      },
+    }),
+    fromAmino: ({ sender, chain, limit: { amount, denom }, window }) => ({
+      sender: toAccAddress(sender),
+      chain,
+      window: {
+        seconds: Long.fromNumber(Number(window) / 1000000000),
+        nanos: Number(window) % 1000000000,
+      },
 
-    // nexus module
-    "/axelar.nexus.v1beta1.SetTransferRateLimitRequest": {
-      aminoType: "nexus/SetTransferRateLimit",
-      toAmino: ({ sender, chain, limit: { amount, denom }, window: { seconds, nanos } }) => ({
-        sender: toBech32(AXELAR_PREFIX, sender),
-        chain,
-        window: Long.fromValue(seconds).multiply(1000000000).add(nanos).toString(),
-        limit: {
-          amount,
-          denom,
-        },
-  
-      }),
-      fromAmino: ({ sender, chain, limit: { amount, denom }, window }) => ({
-        sender: toAccAddress(sender),
-        chain,
-        window: {
-          seconds: Long.fromNumber(Number(window) / (1000000000)),
-          nanos: Number(window) % (1000000000),
-        },
-  
-        limit: {
-          amount,
-          denom,
-        },
-      }),
-    },
-    "/axelar.nexus.v1beta1.ActivateChainRequest": {
-      aminoType: "nexus/ActivateChain",
-      toAmino: ({ sender, chains }) => ({
-        sender: toBech32(AXELAR_PREFIX, sender),
-        chains,
-  
-      }),
-      fromAmino: ({ sender, chains }) => ({
-        sender: toAccAddress(sender),
-        chains,
-      }),
-    },
-    "/axelar.nexus.v1beta1.DeactivateChainRequest": {
-      aminoType: "nexus/DeactivateChain",
-      toAmino: ({ sender, chains }) => ({
-        sender: toBech32(AXELAR_PREFIX, sender),
-        chains,
-  
-      }),
-      fromAmino: ({ sender, chains }) => ({
-        sender: toAccAddress(sender),
-        chains,
-      }),
-    },
-  });
+      limit: {
+        amount,
+        denom,
+      },
+    }),
+  },
+  "/axelar.nexus.v1beta1.ActivateChainRequest": {
+    aminoType: "nexus/ActivateChain",
+    toAmino: ({ sender, chains }) => ({
+      sender: toBech32(AXELAR_PREFIX, sender),
+      chains,
+    }),
+    fromAmino: ({ sender, chains }) => ({
+      sender: toAccAddress(sender),
+      chains,
+    }),
+  },
+  "/axelar.nexus.v1beta1.DeactivateChainRequest": {
+    aminoType: "nexus/DeactivateChain",
+    toAmino: ({ sender, chains }) => ({
+      sender: toBech32(AXELAR_PREFIX, sender),
+      chains,
+    }),
+    fromAmino: ({ sender, chains }) => ({
+      sender: toAccAddress(sender),
+      chains,
+    }),
+  },
+});
