26afe5a463edfd201a916d4f4252e246c2e21fc6
jcs47 <11947034+jcs47@users.noreply.github.com>
2025-03-06 02:54:55 +0000

    feat: add support for multisig module (#351)

---
+++ src/libs/AxelarSigningClient/types/MultisigTxTypes.ts
@@ -0,0 +1,28 @@
+import {
+  StartKeygenRequest,
+  SubmitPubKeyRequest,
+  SubmitSignatureRequest,
+  RotateKeyRequest,
+  KeygenOptInRequest,
+  KeygenOptOutRequest,
+  protobufPackage,
+} from "@axelar-network/axelarjs-types/axelar/multisig/v1beta1/tx";
+import { Registry } from "@cosmjs/proto-signing";
+
+const TxTypeUrlMap = {
+  MultisigStartKeygenRequest: `/${protobufPackage}.StartKeygenRequest`,
+  MultisigKeygenOptInRequest: `/${protobufPackage}.KeygenOptInRequest`,
+  MultisigKeygenOptOutRequest: `/${protobufPackage}.KeygenOptOutRequest`,
+  MultisigRotateKeyRequest: `/${protobufPackage}.RotateKeyRequest`,
+  MultisigSubmitPubKeyRequest: `/${protobufPackage}.SubmitPubKeyRequest`,
+  MultisigSubmitSignatureRequest: `/${protobufPackage}.SubmitSignatureRequest`,
+};
+
+export const registerMultisigTxTypes = (registry: Registry) => {
+  registry.register(TxTypeUrlMap.MultisigStartKeygenRequest, StartKeygenRequest);
+  registry.register(TxTypeUrlMap.MultisigKeygenOptInRequest, KeygenOptInRequest);
+  registry.register(TxTypeUrlMap.MultisigKeygenOptOutRequest, KeygenOptOutRequest);
+  registry.register(TxTypeUrlMap.MultisigRotateKeyRequest, RotateKeyRequest);
+  registry.register(TxTypeUrlMap.MultisigSubmitPubKeyRequest, SubmitPubKeyRequest);
+  registry.register(TxTypeUrlMap.MultisigSubmitSignatureRequest, SubmitSignatureRequest);
+};
